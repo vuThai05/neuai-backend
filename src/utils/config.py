445a2db72aad016b45a34f1cc0a18c1165567db7
vuThai05 <163172997+vuThai05@users.noreply.github.com>
2026-01-28 21:27:49 +0700

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,13 +3,11 @@
 import os
 from pymongo import MongoClient
 
-'''
 MONGO_URI = os.environ.get(
     "MONGODB_URI",
     "mongodb+srv://Neuaiagent_db:rjuhWX6fKqcgVthx@cluster0-newproject.7uk6vxn.mongodb.net/?appName=Cluster0-Newproject",
 )
 MONGO_DB_NAME = "chatbotNeu"
-'''
 
 def get_mongo_client() -> MongoClient:
     """Get MongoDB client instance."""
